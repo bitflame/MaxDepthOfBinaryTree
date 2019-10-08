fff846cf62b587adff12ff51eca9917e6a4d182d
Liquid Falme <sansari1@cox.net>
2019-10-07 23:05:44 -0400

    Successfully submitted to LeetCode.

---
+++ MaxDepthOfBinaryTree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxDepthOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(20);
+            root.right.left = new TreeNode(15);
+            root.right.right = new TreeNode(7);
+            Console.WriteLine(MaxDepth(root));
+            Console.ReadLine();
+        }
+        private static Int32 MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int leftDepth = MaxDepth(root.left);
+            int rightDepth = MaxDepth(root.right);
+
+            return Math.Max(leftDepth, rightDepth)+1;
+        }
+    }
+}
---
+++ MaxDepthOfBinaryTree/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxDepthOfBinaryTree
+{
+    public class TreeNode
+    {
+        public Int32 key;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(Int32 item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+}
